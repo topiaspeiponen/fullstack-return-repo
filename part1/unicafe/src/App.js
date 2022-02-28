1f82553b73e5f341264c1c34b17c519335b1b9b3
topiaspe <topiaspe@metropolia.fi>
2022-02-28 14:46:14 +0200

    excercise 1.8 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,23 +6,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const calculateAvg = () => {
-    const avg = (good - bad) / (good + neutral + bad);
-
-    if (avg) {
-      return avg;
-    }
-    return 0;
-  };
-
-  const calculatePositivePercent = () => {
-      const positivePercent = (good / (good + neutral + bad)) * 100;
-
-      if (positivePercent) {
-        return positivePercent
-      } 
-      return 0;
-  };
+  
 
   return (
     <div>
@@ -32,31 +16,58 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="bad"/>
-
-      <h2>
-        Stats
-      </h2>
-      <p>
-        good {good}
-      </p>
-      <p>
-        neutral {neutral}
-      </p>
-      <p>
-        bad {bad}
-      </p>
-      <p>
-        all {good + neutral + bad}
-      </p>
-      <p>
-        average {calculateAvg()}
-      </p>
-      <p>
-        positive {calculatePositivePercent()} %
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
     </div>
   )
 }
+
+const Statistics = (props) => {
+  const calculateAvg = () => {
+    const avg = (props.good - props.bad) / (props.good + props.neutral + props.bad);
+
+    if (avg) {
+      return avg;
+    }
+    return 0;
+  };
+
+  const calculatePositivePercent = () => {
+      const positivePercent = (props.good / (props.good + props.neutral + props.bad)) * 100;
+
+      if (positivePercent) {
+        return positivePercent
+      } 
+      return 0;
+  };
+
+
+  return (
+    <>
+      <h2>
+          Stats
+        </h2>
+        <p>
+          good {props.good}
+        </p>
+        <p>
+          neutral {props.neutral}
+        </p>
+        <p>
+          bad {props.bad}
+        </p>
+        <p>
+          all {props.good + props.neutral + props.bad}
+        </p>
+        <p>
+          average {calculateAvg()}
+        </p>
+        <p>
+          positive {calculatePositivePercent()} %
+        </p>
+      </>
+  )
+};
 
 const Button = (props) => {
   return (
