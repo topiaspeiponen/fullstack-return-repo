4761ade973fc9a0ea6af01fdfb785ecb032600b4
topiaspe <topiaspe@metropolia.fi>
2022-02-28 11:06:58 +0200

    excercise 1.7 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,24 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const calculateAvg = () => {
+    const avg = (good - bad) / (good + neutral + bad);
+
+    if (avg) {
+      return avg;
+    }
+    return 0;
+  };
+
+  const calculatePositivePercent = () => {
+      const positivePercent = (good / (good + neutral + bad)) * 100;
+
+      if (positivePercent) {
+        return positivePercent
+      } 
+      return 0;
+  };
+
   return (
     <div>
       <h1>
@@ -26,6 +44,15 @@ const App = () => {
       </p>
       <p>
         bad {bad}
+      </p>
+      <p>
+        all {good + neutral + bad}
+      </p>
+      <p>
+        average {calculateAvg()}
+      </p>
+      <p>
+        positive {calculatePositivePercent()} %
       </p>
     </div>
   )
