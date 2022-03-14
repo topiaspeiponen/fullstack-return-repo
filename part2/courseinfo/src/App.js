2acfffee669fc978286fe658ff304c2ad957523a
topiaspe <topiaspe@metropolia.fi>
2022-03-14 18:42:41 +0200

    exercise 2.4 and 2.5  courseinfo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,36 +2,57 @@ import React from 'react'
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "NestJS basics",
+          exercises: 4,
+          id: 4
+        }
+      ]
+  },
+  {
+    name: 'Node.js',
+    id: 2,
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
+        name: 'Routing',
+        exercises: 3,
         id: 1
       },
       {
-        name: 'Using props to pass data',
+        name: 'Middlewares',
         exercises: 7,
         id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "NestJS basics",
-        exercises: 4,
-        id: 4
       }
     ]
   }
+]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )
+      }
     </div>
   )
 }
